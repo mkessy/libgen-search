9525222c5f7acd87db782bf28880c00a02e90f1b
mkessy <kokokessy@gmail.com>
2021-09-14 19:15:26 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+//import { mirror, latest, random, search, utils } from "libgen";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 class="mt-20 mx-auto text-xl font-bold">Welcome to Libgen Search</h1>
     </div>
   );
 }
